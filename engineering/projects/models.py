08dda2b80c691a6df1b4d61965022f5675550873
quantumleeps <dan.leeper@gmail.com>
2017-08-15 19:42:44 -0400

    Complete part 2 of django tutorial

--- engineering/projects/models.py
+++ engineering/projects/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=40)
+    country = models.CharField(max_length=40)
+    projectCode = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.projectCode
+
+class System(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+    systemNumber = models.IntegerField(default=0)
+    systemCode = models.CharField(max_length=4)
+    
+    def __str__(self):
+        return self.systemCode
+
+# class Instrument
+
+# class Valve
+
+# class Equipment
