e4674eb5bb11e7199f4b19081c7cb266b12d6cd4
quantumleeps <dan.leeper@gmail.com>
2017-08-15 22:21:09 -0400

    Customize all model admins

--- engineering/projects/admin.py
+++ engineering/projects/admin.py
@@ -3,7 +3,20 @@ from .models import Project, System, Valve, Instrument
 
 # Register your models here.
 
-admin.site.register(Project)
-admin.site.register(System)
-admin.site.register(Valve)
-admin.site.register(Instrument)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country')
+
+class SystemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'systemNumber')
+
+class ValveAdmin(admin.ModelAdmin):
+    list_display = ('name', 'system', 'pidTagPrefix', 'pidTagNum')
+
+class InstrumentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'system', 'pidTagPrefix', 'pidTagNum')
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(System, SystemAdmin)
+admin.site.register(Valve, ValveAdmin)
+admin.site.register(Instrument, InstrumentAdmin)
