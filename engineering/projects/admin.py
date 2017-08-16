5d6b9d340f51060799cb06cb009a0048eedb2a0c
Dan <Dan@adminisatorsAir.attlocal.net>
2017-08-16 15:57:47 -0400

    Add more models, update instrument model

--- engineering/projects/admin.py
+++ engineering/projects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project, System, Valve, Instrument
+from .models import Project, System, Valve, Instrument, Pump, Tank, Pipe
 
 def duplicate_record(modeladmin, request, queryset):
     for object in queryset:
@@ -48,9 +48,63 @@ class InstrumentAdmin(admin.ModelAdmin):
         # return str(obj.system.systemNumber + self.pidTagNum)
     ordering = ['system__systemNumber']
 
+class PumpAdmin(admin.ModelAdmin):
+    list_display = ('name', 'system', 'system_number', 'pid_tag_number')
+    list_filter = ('system', 'pid_tag_prefix')
+    list_editable = ('system',)
+    actions = [duplicate_record]
+    def system_number(self, obj):
+        return obj.system.systemNumber
+
+    def system_name(self, obj):
+        return obj.system.name
+
+    def pid_tag_number(self, obj):
+        return str(obj.pid_tag_prefix) + '-' + str(obj.system.systemNumber + obj.pid_tag_num - 1)
+        # return str(obj.system.systemNumber + self.pidTagNum)
+    ordering = ['system__systemNumber']
+
+
+class TankAdmin(admin.ModelAdmin):
+    list_display = ('name', 'system', 'system_number', 'pid_tag_number')
+    list_filter = ('system', 'pid_tag_prefix')
+    list_editable = ('system',)
+    actions = [duplicate_record]
+    def system_number(self, obj):
+        return obj.system.systemNumber
+
+    def system_name(self, obj):
+        return obj.system.name
+
+    def pid_tag_number(self, obj):
+        return str(obj.pid_tag_prefix) + '-' + str(obj.system.systemNumber + obj.pid_tag_num - 1)
+        # return str(obj.system.systemNumber + self.pidTagNum)
+    ordering = ['system__systemNumber']
+
+class PipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'system', 'system_number', 'pid_tag_number')
+    list_filter = ('system', 'pid_tag_prefix')
+    list_editable = ('system',)
+    actions = [duplicate_record]
+    def system_number(self, obj):
+        return obj.system.systemNumber
+
+    def system_name(self, obj):
+        return obj.system.name
+
+    def pid_tag_number(self, obj):
+        return str(obj.pid_tag_prefix) + '-' + str(obj.system.systemNumber + obj.pid_tag_num - 1)
+        # return str(obj.system.systemNumber + self.pidTagNum)
+    ordering = ['system__systemNumber']
+
+
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(System, SystemAdmin)
 admin.site.register(Valve, ValveAdmin)
 admin.site.register(Instrument, InstrumentAdmin)
+admin.site.register(Pump, PumpAdmin)
+admin.site.register(Tank, PumpAdmin)
+admin.site.register(Pipe, PumpAdmin)
+
 
 
