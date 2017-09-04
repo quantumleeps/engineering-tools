0de01b3d6ddde66fc504cdf470644de7ae6ca5b5
quantumleeps <dan.leeper@gmail.com>
2017-09-04 14:17:28 -0400

    Update views -perform a cleanup

--- engineering/data_collection/views.py
+++ engineering/data_collection/views.py
@@ -14,24 +14,3 @@ def pick_run(request):
     context = {'runs': runs}
     return render(request, 'data_collection/pick-run.html', context)
 
-def collect_data(request, run_id):
-    run = get_object_or_404(Run, pk=run_id)
-    return render(request, 'data_collection/collect_data.html', {'run': run})
-
-def get_name(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
-
-    return render(request, 'name.html', {'form': form})
