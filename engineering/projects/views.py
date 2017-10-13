aaa9c52ec7fb5425f55da0ae9fb09279fdb235e1
Dan Leeper <dan@Dans-Mac-mini.local>
2017-10-13 10:42:43 -0400

    Show only systems that contain objects that reference them in specific project

--- engineering/projects/views.py
+++ engineering/projects/views.py
@@ -2,6 +2,23 @@ from django.http import HttpResponse
 from .models import Project, System, Instrument, Valve, Pump, Pipe, Tank, Equipment
 from django.shortcuts import get_object_or_404, render
 
+def find_used_systems(systems):
+    used_systems = []
+    for system in systems:
+        if Instrument.objects.filter(system=system).exists():
+            used_systems.append(system)
+        elif Valve.objects.filter(system=system).exists():
+            used_systems.append(system)
+        elif Pump.objects.filter(system=system).exists():
+            used_systems.append(system)            
+        elif Tank.objects.filter(system=system).exists():
+            used_systems.append(system)
+        elif Pipe.objects.filter(system=system).exists():
+            used_systems.append(system)
+        elif Equipment.objects.filter(system=system).exists():
+            used_systems.append(system)
+    return used_systems
+
 def index(request):
     projects = Project.objects.all()
     return render(request, 'projects/home.html', {'projects': projects})
@@ -11,9 +28,12 @@ def tags_list(request, project_slug, system_slug=None, tag_prefix=None):
     kwargs = {
         'project__slug': project_slug,
     }
-
+    # currently there's a bug that if you select a system when adding an instrument that isn't in
+    # the project, it'll still display on the page. Need to filter what shows on the page to only
+    # be systems that are in the project's system list
+    # need to check that the instrument's system is part of the project's systems:
     contents = {}
-    contents['systems'] = Project.objects.filter(slug=project_slug)[0].systems.all()
+    contents['systems'] = find_used_systems(get_object_or_404(Project, slug=project_slug).systems.all())
     contents['project_slug'] = project_slug
     contents['project_name'] = get_object_or_404(Project, slug=project_slug)
     contents['tag_prefix'] = tag_prefix
