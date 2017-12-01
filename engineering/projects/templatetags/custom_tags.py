34200b9b5be5931984be29dd0c10411f657d2a19
quantumleeps <dan.leeper@gmail.com>
2017-12-01 07:06:58 -0500

    Change font size of something on controlled doc list

--- engineering/projects/templatetags/custom_tags.py
+++ engineering/projects/templatetags/custom_tags.py
@@ -9,11 +9,11 @@ def process_system(system, current_system, current_category):
     kwargs['system__slug'] = system['system__slug']
     kwargs['category__slug'] = current_category
     if not current_system and not current_category:
-        return '<a href="?system=' + system['system__slug'] + '" class="filter-button btn btn-primary active" role="button">' + system['system__name'] + '</a><br>'
+        return '<a href="?system=' + system['system__slug'] + '" class="filter-button btn btn-primary .btn-sm active" role="button">' + system['system__name'] + '</a><br>'
     if system['system__slug'] == current_system:
-        return '<a href="#" class="filter-button btn btn-success btn-outline inactive" role="button">'+ system['system__name'] + '</a><br>'
+        return '<a href="#" class="filter-button btn btn-success btn-outline .btn-sm inactive" role="button">'+ system['system__name'] + '</a><br>'
     elif ControlledDocument.objects.filter(**kwargs).exists() == True:
-        return '<a href="?category=' + current_category + '&system=' + system['system__slug'] + '" class="filter-button btn btn-primary active" role="button">' + system['system__name'] + '</a><br>'
+        return '<a href="?category=' + current_category + '&system=' + system['system__slug'] + '" class="filter-button btn btn-primary .btn-sm active" role="button">' + system['system__name'] + '</a><br>'
     else: 
         return '<a href="#" class="filter-button btn btn-primary disabled" role="button">'+ system['system__name'] + '</a><br>'
 
@@ -27,13 +27,8 @@ def process_category(category, current_category, current_system):
     if category['category__slug'] == current_category:
         return '<a href="#" class="filter-button btn btn-success inactive" role="button">'+ category['category__name'] + '</a><br>'
     elif ControlledDocument.objects.filter(**kwargs).exists() == True:
-        
         # If there are matching documents, you want an active link to be generated
-
-        
-        
         return '<a href="?category=' + category['category__slug'] + '&system=' + current_system + '" class="filter-button btn btn-primary active" role="button">' + category['category__name'] + '</a><br>'
     else: 
-
         # If there are no matchign documents, it should be displayed but not as a link 
         return '<a href="#" class="filter-button btn btn-primary disabled" role="button">'+ category['category__name'] + '</a><br>'
